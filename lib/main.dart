9b64f33df47f5fd93e82fbae08cb8bb8d9fba5f2
Andy <ltanh1194@gmail.com>
2021-02-24 11:42:07 +0700

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:infinitelistapp/blocs/comment_bloc.dart';
+import 'package:infinitelistapp/events/comment_events.dart';
 import 'package:infinitelistapp/infinite_list.dart';
 
 void main() {
@@ -10,8 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: InfiniteList(),
-    );
+        title: 'Flutter Demo',
+        home: BlocProvider(
+          create: (context) => CommentBloc()..add(CommentFetchedEvent()),
+          child: InfiniteList(),
+        ));
   }
 }
